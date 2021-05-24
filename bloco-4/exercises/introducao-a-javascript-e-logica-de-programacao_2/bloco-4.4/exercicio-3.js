55c9a7fa9bf8ec9ca0765ec5cfb9669e0f834f4a
Caê <eduardokae01@hotmail.com>
2021-05-24 18:53:25 -0300

    usando funçoes nos exercicios 4.2 ate exercicio 5

---
+++ bloco-4/exercises/introducao-a-javascript-e-logica-de-programacao_2/bloco-4.4/exercicio-3.js
@@ -0,0 +1,98 @@
+// //exercicio 1
+
+// function sum(a, b){
+//   return a + b;
+// };
+
+// console.log(sum(5, 25));
+
+// function subtraction(a, b){
+//   return a - b;
+// };
+
+// console.log(subtraction(30, 10));
+
+// function multiplication(a, b){
+//   return a * b;
+// };
+
+// console.log(multiplication(2, 10));
+
+// function division(a, b){
+//   return a / b;
+// };
+
+// console.log(division(100, 10));
+
+// function module(a, b){
+//   return a % b;
+// };
+
+// console.log(module(10, 2));
+
+// //exercicio 2
+
+// function bigger(a, b){
+//   if(a > b){
+//     console.log(a);
+
+//   } else {
+//     console.log(b);
+//   }
+//   return console.log;
+// };
+
+// bigger(1150, 250);
+
+// // Exxercicio 3
+
+// function evenBig(a, b, c){
+//   if(a > b && a > c){
+//     console.log(a);
+//   }
+//   else if(b > a && b > c){
+//     console.log(b);
+
+//   } else {
+//     console.log(c);
+//   }
+  
+//   return console.log;
+// };
+
+// evenBig (45, 67, 18);
+
+// // Exercicio 4
+
+// function polarity(a){
+//   if(a > 0){
+//     console.log("positive");
+//   }
+//   else if(a < 0){
+//     console.log("negative");
+
+//   } else {
+//     console.log("zero");
+//   }
+
+//   return console.log;
+// };
+
+// polarity(57);
+// polarity(-150);
+// polarity(0);
+
+// Exercicio 5
+
+function triangule(a, b, c){
+  if(a + b + c === 180){
+    console.log("true");
+  
+  } else {
+    console.log("false");
+  }
+  
+  return(console.log);
+}
+
+triangule(20, 40, 60);
