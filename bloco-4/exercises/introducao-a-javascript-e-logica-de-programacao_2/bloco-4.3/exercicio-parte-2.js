3267caf855ffed513c3db601c5b98e7e957bb6e1
Caê <eduardokae01@hotmail.com>
2021-05-25 11:38:27 -0300

    transforma exercicios piramide em funcoes

---
+++ bloco-4/exercises/introducao-a-javascript-e-logica-de-programacao_2/bloco-4.3/exercicio-parte-2.js
@@ -0,0 +1,49 @@
+
+// let linha = " ";
+
+// function quadrado(n){
+//   for(let index = 0; index < n; index +=1){
+//     linha = linha + "*" + " " + " ";
+//   }
+//   for(index = 0; index < n; index +=1){
+//     console.log(linha);
+//   }
+// };
+
+// quadrado(5);
+
+// //Exercicio 2
+
+// let linha = " ";
+
+// function piramide(n){
+//   for (let index = 0; index < n; index +=1){
+//     linha = linha + "*" + " " + " ";
+//     console.log(linha);
+//   }
+// };
+
+// piramide(8);
+
+// // // Exercicio 3
+
+// let linha = " ";
+
+// function pirInvert(a){
+//   let espaco = a;
+//   for(let index = 0; index <= a; index += 1) {
+//     for(let coluna = 0; coluna <= a; coluna +=1) {
+//       if( coluna < espaco) {
+//     linha = linha + " ";
+
+//       } else {
+//         linha = linha + "*";
+//       }
+//     }
+//     console.log(linha);
+//     linha = " ";
+//     espaco -= 1;
+//   } 
+// }
+
+// pirInvert(10);
---
+++ bloco-4/exercises/introducao-a-javascript-e-logica-de-programacao_2/bloco-4.4/exercicio-5.js
@@ -0,0 +1,16 @@
+function palindrome(word){
+  for(let index = word.length; index >= 0; index -= 1){
+    let palind = word[index];
+  }
+  if(a === palind){
+    console.log("true");
+
+  } else {
+    console.log("false");
+  }
+
+  return (console.log);
+};
+
+console.log(palindrome("arara"));
+
