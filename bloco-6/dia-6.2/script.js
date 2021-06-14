d3b518e9bea8e320e9ca544171d909ae33db2845
Caê <eduardokae01@hotmail.com>
2021-06-14 18:20:08 -0300

    iniciando arquivo html e css

--- bloco-6/dia-6.2/script.js
+++ bloco-6/dia-6.2/script.js
@@ -1,9 +1,10 @@
 const botao = document.querySelector('#botao-submit');
 let container = document.querySelector('.conteiner-1');
+const inputEstados = document.getElementById('input-estados');
+
 
 botao.addEventListener('click' function (event) {
-  event.preventDefault();
-  
+  event.preventDefault();  
 });
 
 const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 
@@ -14,3 +15,15 @@ const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Di
 const siglas = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 
 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
 
+let listaEstados = '';
+
+
+for (let contagem = 0; contagem < estados.length; contagem += 1) {
+  let listaEstados = estados[contagem];
+  let listaSiglas = siglas[contagem];
+
+  let opcaoSelected = document.createElement('option');
+  opcaoSelected.innerText = listaEstados;
+  inputEstados.appendChild(opcaoSelected);
+}
+console.log(listaEstados);
