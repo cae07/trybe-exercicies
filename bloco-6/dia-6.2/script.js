79de23af5fbc2ace1fe987792ac4d259e6468cb6
Caê <eduardokae01@hotmail.com>
2021-06-14 13:59:49 -0300

    acrescenta array estados e siglas

--- bloco-6/dia-6.2/script.js
+++ bloco-6/dia-6.2/script.js
@@ -5,3 +5,12 @@ botao.addEventListener('click' function (event) {
   event.preventDefault();
   
 });
+
+const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 
+'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 
+'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 
+'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+
+const siglas = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 
+'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+
