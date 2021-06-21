9f64aec704dc0ef628218cd021a68f145681a70f
Caê <eduardokae01@hotmail.com>
2021-06-21 19:10:27 -0300

    para mudança de branch

--- bloco-7/script.js
+++ bloco-7/script.js
@@ -19,7 +19,7 @@ oddsAndEvens.sort((a,b) => a - b);
 
 console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente.`);
 
-oddsAndEvens.sort((a,b) => b-a);
+oddsAndEvens.sort((a,b) => b - a);
 console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma decrescente.`);
 
 // Exercicio 3 arrow function comum
