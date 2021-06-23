7b8bcb1a2b180ac3164f74fa25e596d369f1617a
Caê <eduardokae01@hotmail.com>
2021-06-23 12:14:32 -0300

    modificando concatenações para template literals

--- bloco-7/dia-7.1/script.js
+++ bloco-7/dia-7.1/script.js
@@ -2,14 +2,17 @@
 const testingScope = (escopo) => {
   if (escopo === true) {
     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
     console.log(ifScope);
   } else {
     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
     console.log(elseScope);
   }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O');
+  console.log(`${ifScope} o que estou fazendo aqui ? :O`);
 }
 
 
 testingScope(true);
+
+//  Exercicio 2 - Parte 1
+
