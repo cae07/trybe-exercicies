2d9ff64771b8ff6368954b1e6450066989719ea4
Caê <eduardokae01@hotmail.com>
2021-07-09 11:32:03 -0300

    bloco 7 - dia 7.2 / bloco 8 - dia 8.2

---
+++ bloco-8/dia-8.3/exercicio.js
@@ -0,0 +1,86 @@
+//  Exercicio 1
+const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+const prices = [2.99, 3.99, 1.5, 2];
+
+const productWithPrice = (productList, priceList) => products.map((product, index) => (
+  { [product]: priceList[index] }
+));
+
+console.log(productWithPrice(products, prices));
+
+//  Exemplo 2
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: '59' },
+      { name: 'Português', nota: '80' },
+      { name: 'Química', nota: '78' },
+      { name: 'Biologia', nota: '92' },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '76' },
+      { name: 'Português', nota: '90' },
+      { name: 'Química', nota: '70' },
+      { name: 'Biologia', nota: '80' },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '91' },
+      { name: 'Português', nota: '85' },
+      { name: 'Química', nota: '92' },
+      { name: 'Biologia', nota: '90' },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '70' },
+      { name: 'Português', nota: '70' },
+      { name: 'Química', nota: '60' },
+      { name: 'Biologia', nota: '50' },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '80' },
+      { name: 'Português', nota: '82' },
+      { name: 'Química', nota: '79' },
+      { name: 'Biologia', nota: '75' },
+    ],
+  },
+];
+
