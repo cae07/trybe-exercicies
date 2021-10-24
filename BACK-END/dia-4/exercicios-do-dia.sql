5ea5c91b405ea0841682f44d6a5ee22706b34258
Cae <eduardokae01@hotmail.com>
2021-10-23 21:26:31 -0300

    exercicio 4

--- BACK-END/dia-4/exercicios-do-dia.sql
+++ BACK-END/dia-4/exercicios-do-dia.sql
@@ -20,5 +20,18 @@ INNER JOIN Pixar.BoxOffice B
 ON P.id = B.movie_id
 ORDER BY B.rating DESC;
 
+-- Exercício 4: Utilizando o LEFT JOIN , faça uma busca que retorne todos os dados dos cinemas,
+-- mesmo os que não possuem filmes em cartaz e, adicionalmente, os dados dos filmes que estão
+-- em cartaz nestes cinemas. Retorne os nomes dos cinemas em ordem alfabética.
+SELECT
+	theater.*, movies.*
+FROM Pixar.Theater AS theater
+LEFT JOIN Pixar.Movies AS movies
+ON theater.id = movies.theater_id
+ORDER BY movies.title;
+
+
+
+
 
 
