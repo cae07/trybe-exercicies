0fa39753e47d62931eae77ace7fe965accc8d494
Cae <eduardokae01@hotmail.com>
2021-10-23 21:20:29 -0300

    exercicio 3

--- BACK-END/dia-4/exercicios-do-dia.sql
+++ BACK-END/dia-4/exercicios-do-dia.sql
@@ -13,6 +13,12 @@ INNER JOIN Pixar.BoxOffice AS B
 ON P.id = B.movie_id
 WHERE B.domestic_sales < B.international_sales;
 
+-- Exercício 3: Utilizando o INNER JOIN , faça uma busca que retorne os filmes
+-- e sua avaliação ( rating ) em ordem decrescente.
+SELECT P.title, B.rating FROM Pixar.Movies P
+INNER JOIN Pixar.BoxOffice B
+ON P.id = B.movie_id
+ORDER BY B.rating DESC;
 
 
 
