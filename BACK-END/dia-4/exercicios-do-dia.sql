377d8a20bb33ad1233d178a0f7be63d91b663193
Cae <eduardokae01@hotmail.com>
2021-10-23 21:45:23 -0300

    exercicio 5

--- BACK-END/dia-4/exercicios-do-dia.sql
+++ BACK-END/dia-4/exercicios-do-dia.sql
@@ -30,7 +30,15 @@ LEFT JOIN Pixar.Movies AS movies
 ON theater.id = movies.theater_id
 ORDER BY movies.title;
 
-
+-- Exercício 5: Utilizando o RIGHT JOIN , faça uma busca que retorne todos os dados
+-- dos filmes, mesmo os que não estão em cartaz e, adicionalmente, os dados dos cinemas
+-- que possuem estes filmes em cartaz. Retorne os nomes dos cinemas em ordem alfabética.
+SELECT
+	theater.*, movies.*
+FROM Pixar.Theater AS theater
+RIGHT JOIN Pixar.Movies AS movies
+ON theater.id = movies.theater_id
+ORDER BY theater.name;
 
 
 
