2728ccc4970c56c5280d27de5527fac35caac745
Cae <eduardokae01@hotmail.com>
2021-11-25 15:41:37 -0300

    exemplo de aplicando contições ao JOIN com lookup

---
+++ testes/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+const array = [1, 3, 5, 4, 0, 0, 7, 0, 0, 6]
+
+for (let i = 0; i < array.length; i += 1) {
+  for (let y = 0; y < array.length; i += 1) {
+    console.log(`i = ${i}`);
+    console.log(`y = ${y}`);
+  }
+}
