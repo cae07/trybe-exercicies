6e7f1e1d830d92824f99fab0727869a824b13624
Cae <eduardokae01@hotmail.com>
2021-12-01 17:56:52 -0300

    exercicio dia 2

--- BACK-END/dia-26.2/index.js
+++ BACK-END/dia-26.2/index.js
@@ -23,6 +23,29 @@ function calcula(num1, num2, num3) {
   return promise;
 };
 
-calcula(10, 2, 1)
-  .then((response) => console.log(response))
-  .catch((error) => console.log(error.message));
+//  2. Escreva um código para consumir a função construída no exercício anterior.
+// 2.1 - Gere um número aleatório de 1 a 100 para cada parâmetro que a função recebe.
+// Para gerar um número aleatório, utilize o seguinte trecho de código: 
+// 2.2 - Chame a função do exercício anterior, passando os três números aleatórios como parâmetros.
+// Math.floor(Math.random() * 100 + 1) .
+// 2.3 - Utilize then e catch para manipular a Promise retornada pela função:
+
+// 2.3.1 - Caso a Promise seja rejeitada, escreva na tela o motivo da rejeição.
+// 2.3.2 - Caso a Promise seja resolvida, escreva na tela o resultado do cálculo.
+function randomNumber() {
+  const num1 = Math.floor(Math.random() * 100 + 1);
+  const num2 = Math.floor(Math.random() * 100 + 1);
+  const num3 = Math.floor(Math.random() * 100 + 1);
+
+  console.log(`num1 = ${num1}`);
+  console.log(`num2 = ${num2}`);
+  console.log(`num3 = ${num3}`);
+
+  calcula(num1, num2, num3)
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err.message));
+};
+
+randomNumber()
+
+
