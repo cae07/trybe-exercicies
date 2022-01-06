7f693e5977732252911b09076ce99c139f9934cd
Cae <eduardokae01@hotmail.com>
2022-01-06 11:29:37 -0300

    atualizando a branch

---
+++ BACK-END/dia-26.4/hello-express/exemplo-express-hello-world.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express(); // 1
+
+app.get('/hello', handleHelloWorldRequest); // 2
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+}); // 3
+
+function handleHelloWorldRequest(req, res, next) {
+  // esse é nosso middleWare, recebe 3 params
+  res.status(200).send("Olá mundo com o Caê"); // 4
+}
+
+// Esse pequeno script é o suficiente para:
+// 1. Criar uma nova aplicação Express;
+
+// 2. Dizer ao Express que, quando uma requisição com método GET for
+// recebida no caminho /hello , a função handleHelloWorldRequest
+// deve ser chamada;
+// 3. Pedir ao Express que crie um servidor HTTP e escute por
+// requisições na porta 3001;
+// 4 .Ao tratar uma requisição com método GET no caminho /hello ,
+// enviar o status HTTP 200 que significa OK e a mensagem
+// "Hello world!".
