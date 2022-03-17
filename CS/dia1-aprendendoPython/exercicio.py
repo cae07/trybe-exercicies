77b24de2c19c49132e04446f674c3411b402877a
Cae <eduardokae01@hotmail.com>
2022-03-17 08:36:17 -0300

    typescript

---
+++ CS/dia1-aprendendoPython/exercicio.py
@@ -0,0 +1,14 @@
+fruits = ["banana", "maçã", "pera"]
+
+print(fruits)
+
+fruits.append('laranja')
+print(fruits)
+
+fruits.extend(["lima", 'abacaxi', 'uva'])
+print(fruits)
+
+fruits.sort()
+print(fruits)
+# number=[n for n in range(1, 11)]
+# print(number)
