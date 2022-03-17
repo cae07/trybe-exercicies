40e038b3851b7b46f81d7ac2de74ef8b01d0e257
Cae <eduardokae01@hotmail.com>
2022-03-17 08:48:27 -0300

    feat: operações basicas em python

---
+++ CS/dia1-aprendendoPython/operacoesBasicas.py
@@ -0,0 +1,10 @@
+a = 20
+b = 10
+
+print(a + b) # soma => saida 30
+print(a - b) # subtação => saida 10
+print(a * b) # multiplicação => saida 200
+print(a / b) # divisão => saida 2.0
+print(a // b) # quociente (arredonda pra baixo) => saida 2
+print(a ** b) # exponenciação => saida 10240000000000
+print(a ** (1/2)) # raiz quadrada => saida 4.47213595499958
