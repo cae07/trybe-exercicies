677db5101e5be3a1ed84eb35791097f745744c6e
Cae <eduardokae01@hotmail.com>
2022-03-17 09:30:06 -0300

    feat: listas em python

---
+++ CS/dia1-aprendendoPython/listas.py
@@ -0,0 +1,15 @@
+fruits = ["orange", "apple", "grape", "pineapple"]
+
+fruits[0]  # o acesso é feito por indices iniciados em 0
+
+fruits[-1]  # o acesso também pode ser negativo
+
+fruits.append("banana")  # adicionando uma nova fruta
+
+fruits.remove("pineapple")  # removendo uma fruta
+
+fruits.extend(["pear", "melon", "kiwi"])  # acrescenta uma lista de frutas a lista original
+
+fruits.index("apple")  # retorna o índice onde a fruta está localizada, neste caso 1 em seu programa
+
+fruits.sort()  # ordena a lista de frutas
