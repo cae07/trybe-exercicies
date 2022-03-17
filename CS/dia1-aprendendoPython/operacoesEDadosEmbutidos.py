737fd182bfa8874cbd48d82967013948bb795f40
Cae <eduardokae01@hotmail.com>
2022-03-17 08:56:44 -0300

    feat: dados embutidos em python

---
+++ CS/dia1-aprendendoPython/operacoesEDadosEmbutidos.py
@@ -0,0 +1,32 @@
+# OPERAÇÕES BÁSICAS
+# a = 20
+# b = 10
+
+# print(a + b) # soma => saida 30
+# print(a - b) # subtação => saida 10
+# print(a * b) # multiplicação => saida 200
+# print(a / b) # divisão => saida 2.0
+# print(a // b) # quociente (arredonda pra baixo) => saida 2
+# print(a ** b) # exponenciação => saida 10240000000000
+# print(a ** (1/2)) # raiz quadrada => saida 4.47213595499958
+
+# DADOS EMBUTIDOS
+
+# NÚMEROS
+# a = 10
+# print(type(a)) # saida <class 'int'>
+
+# b = 5.0
+# print(type(b)) # saida <class 'float'>
+# 
+# c = 5 + 6j
+# print(c) # saida (5+6j)
+# print(type(c)) # saida <class 'complex'>
+
+# OUTROS
+
+a = 'string'
+print(type(a)) # saida <class 'str'>
+
+b = True # booleanos precisam começar maiúsculo
+print(type(b)) # saida <class 'bool'>
