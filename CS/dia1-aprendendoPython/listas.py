e58dd93966f2105c7d929b6238f8cbc3d82c3ff8
Cae <eduardokae01@hotmail.com>
2022-03-17 10:22:46 -0300

    feat: dicts

--- CS/dia1-aprendendoPython/listas.py
+++ CS/dia1-aprendendoPython/listas.py
@@ -39,9 +39,35 @@ permissions.add("member")  # se o elemento já existir não faz nada
 
 permissions.union({"user"})  # faz a união de outro set
 
-print(
-    permissions.intersection({"user", "member"})
-)  # retorna um conjunto resultante da intersecção dos conjuntos
+#print(
+#    permissions.intersection({"user", "member"})
+#)  # retorna um conjunto resultante da intersecção dos conjuntos
 
 
-print(permissions.difference({"user"}))  # retorna a diferença entre os dois conjuntos
+#print(permissions.difference({"user"}))  # retorna a diferença entre os dois conjuntos
+
+##### FROZENSET => IMUTÁVEIS #####
+imutaveis = frozenset(["member", "group"])  # assim como o set, qualquer estrutura iterável pode ser utilizada para criar um frozenset
+print(imutaveis)
+
+print(imutaveis.union({"user"}))  # novos conjuntos imutáveis podem ser criados à partir do original, mas o mesmo não pode ser modificado
+
+print(imutaveis.intersection({"user", "member"}))  # retorna um conjunto resultante da intersecção dos conjuntos
+
+print(imutaveis.difference({"user"}))  # retorna a diferença entre os dois conjuntos
+
+
+##### DICT / REPRESENTAÇÃO CHAVE => VALOR #####
+
+people_by_id = {1: "Cássio", 2: "João", 3: "Felipe"}  # elementos no formato "chave: valor" separados por vírgula, envolvidos por chaves
+
+people_by_name = {"Cássio": 1, "João": 2, "Felipe": 3}  # outro exemplo, dessa vez usando strings como chaves (ao contrário de JS, as aspas duplas são obrigatórias)
+
+# elementos são acessados por suas chaves
+people_by_id[1]  # saída: Cássio
+
+# elementos podem ser removidos com a palavra chave del
+del people_by_id[1]
+
+print(people_by_id.items())  # dict_items([(1, "Cássio"), (2, "João"), (3, "Felipe")])
+# um conjunto é retornado com tuplas contendo chaves e valores
