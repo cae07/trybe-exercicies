3d7e03230bd95ab479116a432edf38a0bdb71b77
Cae <eduardokae01@hotmail.com>
2022-03-17 10:15:46 -0300

    feat: exercicio 7

--- CS/dia1-aprendendoPython/exercicio.py
+++ CS/dia1-aprendendoPython/exercicio.py
@@ -7,3 +7,13 @@ trybe_course.append("Ciência da Computação")
 trybe_course[0].replace("Introdução", "Fundamentos")
 
 print(trybe_course)
+
+# Exercício 7: Um conjunto ou set pode ser inicializado utilizando-se
+# também o método set() . Inicialize uma variável com essa função
+# var = set() e adicione seu nome ao conjunto utilizando um dos métodos
+# vistos acima. Depois, imprima a variável e confira se o retorno é: {'seu_nome'}.
+
+var = set()
+
+var.add("Caê")
+print(var)
