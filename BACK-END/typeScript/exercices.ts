e8877ba6c4f3e80b224f6f3cfcdf7c35448985e5
Cae <eduardokae01@hotmail.com>
2022-05-23 11:20:42 -0300

    feat: função que recebe uma string e um number e retorna string

--- BACK-END/typeScript/exercices.ts
+++ BACK-END/typeScript/exercices.ts
@@ -1,3 +1,8 @@
 export function greeter(name: string): string {
   return `Olá ${name}`;
 }
+
+export function personAge(name: string, age: number): string {
+  return `${name} tem ${age} anos de idade.`;
+}
+
