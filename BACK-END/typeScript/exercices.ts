3fc32a285abd4020009a67ad92cda54cb241ef87
Cae <eduardokae01@hotmail.com>
2022-05-23 11:41:22 -0300

    feat: calculo de areas triangulo, quadrado, retangulo

--- BACK-END/typeScript/exercices.ts
+++ BACK-END/typeScript/exercices.ts
@@ -6,6 +6,7 @@ export function personAge(name: string, age: number): string {
   return `${name} tem ${age} anos de idade.`;
 }
 
+// auxiliar para somar o array
 export function add(x: number, y: number): number {
   return x + y;
 }
@@ -13,3 +14,23 @@ export function add(x: number, y: number): number {
 export function sumArray(numbers: number[]): number {
   return numbers.reduce(add, 0);
 }
+
+// calcula a área do triangulo
+export function triangle(base: number, height: number): number {
+  return (base * height) / 2;
+}
+
+// calcula a área do quadrado
+export function square(side: number): number {
+  return side ** 2;
+}
+
+// exemplo em arrow function com export a parte;
+// calcula a área do retangulo
+const rectangle = (base: number, height: number): number => {
+  return base * height;
+}
+
+export {
+  rectangle
+};
