c397901591e08a1d05f69fec6342e2546880a51c
Cae <eduardokae01@hotmail.com>
2022-05-23 11:48:19 -0300

    feat: testa nova função

--- BACK-END/typeScript/index.ts
+++ BACK-END/typeScript/index.ts
@@ -15,3 +15,7 @@ console.log(`Quadrado de lado 100cm: ${Exercise.square(10)}cm²`);
 console.log(`Retângulo de base 10cm e altura 25cm: ${Exercise.rectangle(10, 25)}cm²`);
 console.log(`Retângulo de base 5cm e altura 30cm: ${Exercise.rectangle(5, 30)}cm²`);
 console.log(`Retângulo de base 100cm e altura 200cm: ${Exercise.rectangle(10, 25)}cm²`);
+
+console.log(`Losango de diagonal maior 32cm e diagonal menor 18cm: ${Exercise.rectangle(32, 18)}cm²`);
+console.log(`Losango de diagonal maior 200cm e diagonal menor 50cm: ${Exercise.rectangle(200, 50)}cm²`);
+console.log(`Losango de diagonal maior 75cm e diagonal menor 25cm: ${Exercise.rectangle(75, 25)}cm²`);
