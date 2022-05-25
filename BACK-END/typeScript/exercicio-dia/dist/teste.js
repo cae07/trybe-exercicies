4db346977331edd4bfbd12d45d0a9fdf38ff3569
Cae <eduardokae01@hotmail.com>
2022-05-25 15:40:23 -0300

    compila

--- BACK-END/typeScript/exercicio-dia/dist/teste.js
+++ BACK-END/typeScript/exercicio-dia/dist/teste.js
@@ -7,3 +7,18 @@ var StudentStatus;
 })(StudentStatus || (StudentStatus = {}));
 let newStudentStatus = StudentStatus.Inactive;
 console.log(`O status do estudante é: ${newStudentStatus}`);
+var StatusCodes;
+(function (StatusCodes) {
+    StatusCodes[StatusCodes["OK"] = 200] = "OK";
+    StatusCodes[StatusCodes["BadRequest"] = 400] = "BadRequest";
+    StatusCodes[StatusCodes["Unauthorized"] = 401] = "Unauthorized";
+    StatusCodes[StatusCodes["PaymentRequired"] = 402] = "PaymentRequired";
+    StatusCodes[StatusCodes["Forbidden"] = 403] = "Forbidden";
+    StatusCodes[StatusCodes["NotFound"] = 404] = "NotFound";
+})(StatusCodes || (StatusCodes = {}));
+const ok = StatusCodes.OK;
+const indiceOk = StatusCodes["OK"];
+const stringBadRequest = StatusCodes[400];
+console.log(ok); //saída: 200
+console.log(indiceOk); //saída: 200
+console.log(stringBadRequest); //saída: BadRequest
