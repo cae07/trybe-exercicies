d60b0e3a9e3daa6128d4f27c979e948249e1ea7a
Cae <eduardokae01@hotmail.com>
2022-05-25 15:33:52 -0300

    feat: exercicio 1

---
+++ BACK-END/typeScript/exercicio-dia/exerciciosEnum.ts
@@ -0,0 +1,30 @@
+// Crie uma Enum que represente os dias da semana. Seu valor deve ser número do dia.
+enum DaysOfWeek {
+  domingo = 1,
+  segunda,
+  terca,
+  quarta,
+  quinta,
+  sexta,
+  sabado,
+};
+
+const quartaFeira = DaysOfWeek["quarta"]; // saida => 4
+const quartaFeiraString = DaysOfWeek[4]; // saida => quarta
+
+console.log(quartaFeira);
+console.log(quartaFeiraString);
+
+
+
+
+// Crie uma Enum que represente as cores do arco iris. Seu valor deve ser o nome das cores em português.
+
+
+// Crie uma Enum que represente as ações: salvar, imprimir, abrir, visualizar e fechar. Seu valor deve ser do tipo inteiro.
+
+
+
+// Crie uma Enum que represente os pontos cardeais: Norte, Leste, Sul e Oeste. Seu valor deve ser a primeira letra do nome do ponto cardial.
+
+
