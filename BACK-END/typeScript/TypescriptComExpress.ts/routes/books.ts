4156a49f98ac7ba418100433cde0d44a02fa7b59
Cae <eduardokae01@hotmail.com>
2022-05-26 20:22:00 -0300

    refactor: rota get raiz

--- BACK-END/typeScript/TypescriptComExpress.ts/routes/books.ts
+++ BACK-END/typeScript/TypescriptComExpress.ts/routes/books.ts
@@ -1,9 +1,15 @@
 import { Router, Request, Response } from 'express';
+import fs from 'fs/promises';
+import Book from "../interfaces/Book";
 
 const router = Router();
 
-router.get("/books", (req: Request, res: Response) => {
+router.get("/books", async (req: Request, res: Response) => {
+  const data = await fs.readFile("./books.json", "utf8");
 
+  const books: Book[] = JSON.parse(data);
+
+  return res.status(200).json(books);
 });
 
 router.get("/books/:isbn", (req: Request, res: Response) => {
