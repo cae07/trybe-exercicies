fd5d2ec182efbcd0752bc266b02fb9123ec2b797
Cae <eduardokae01@hotmail.com>
2022-05-26 15:51:54 -0300

    exercicio do dia 2

---
+++ BACK-END/typeScript/exercicio-dia/EstacoesDoAno.ts
@@ -0,0 +1,11 @@
+// Exercício 2
+// Crie uma enum que represente as estações climáticas do ano.
+
+enum EstacoesDoAno {
+  SPRING = "Primavera",
+  SUMMER = "Verão",
+  FALL = "Outono",
+  WINTER = "Inverno",
+}
+
+export default EstacoesDoAno;
