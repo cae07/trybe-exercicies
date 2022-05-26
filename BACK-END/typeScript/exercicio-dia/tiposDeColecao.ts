c0f8cfb17a9bbda4382a6063cbcbaf291d7afc09
Cae <eduardokae01@hotmail.com>
2022-05-26 15:43:56 -0300

    tipos de coleção

--- BACK-END/typeScript/exercicio-dia/tiposDeColecao.ts
+++ BACK-END/typeScript/exercicio-dia/tiposDeColecao.ts
@@ -22,10 +22,23 @@ console.log(sum({x: 10, y: 20}));
 
 
 // Crie um type para um objeto que represente um endereço.
-type Adrees = {
+type Address = {
   rua: string;
   bairro: string;
   cidade: string;
   estado: string;
   cep: string;
 }
+
+// Crie um type union que represente os estados físicos da matéria: líquido, sólido ou gasoso.
+type EstadosDaMateria = "liquido" | "solido" | "gasoso";
+
+// Crie um type union que represente o documento identificador de uma pessoa
+// que pode receber valores numéricos ou texto. Ex: “123.567.890-12” ou 123456789012.
+type cpf = string | number;
+
+// Crie um type union que represente sistemas operacionais: linux, mac os ou windows.
+type sistemaOperacional = "linux" | "mac" | "windows";
+
+// Crie um type union que represente as vogais do alfabeto.
+type alphabet = "a" | "e" | "i" | "o" | "u";
