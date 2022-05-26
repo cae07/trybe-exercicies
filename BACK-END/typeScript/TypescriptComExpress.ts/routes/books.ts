35dfbe9e472ca8c6453506a90d030f88cfa7a924
Cae <eduardokae01@hotmail.com>
2022-05-26 20:16:06 -0300

    feat: inicia rotas para primeiro CRUD

---
+++ BACK-END/typeScript/TypescriptComExpress.ts/routes/books.ts
@@ -0,0 +1,25 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get("/books", (req: Request, res: Response) => {
+
+});
+
+router.get("/books/:isbn", (req: Request, res: Response) => {
+
+});
+
+router.post("/books", (req: Request, res: Response) => {
+
+});
+
+router.put("/books/:isbn", (req: Request, res: Response) => {
+
+});
+
+router.delete("/books/:isbn", (req: Request, res: Response) => {
+
+});
+
+export default router;
