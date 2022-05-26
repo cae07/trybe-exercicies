cfd5ef51ffda319e44b2899416868be6e2d5564e
Cae <eduardokae01@hotmail.com>
2022-05-26 20:49:01 -0300

    feat: middleware que faz o parse dos req.body

--- BACK-END/typeScript/TypescriptComExpress.ts/index.ts
+++ BACK-END/typeScript/TypescriptComExpress.ts/index.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response} from 'express';
 import BooksRoutes from "./routes/books";
 
-const app = express();
+const app: express.Application = express();
 
 const PORT = 8000;
+
+app.use(express.json());
 
 app.get('/', (_req: Request, res: Response) => {
   res.send("express + TypeScript")
